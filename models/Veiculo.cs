89e304309a625ee882bf95e5ee4b966c7306003c
GuilhermeJuli <guilhermemoraes.julio@hotmail.com>
2024-08-08 00:35:21 -0300

    Commit Inicial com a estruturação de pastas e models.veiculo

---
+++ models/Veiculo.cs
@@ -0,0 +1,10 @@
+namespace ApiVeiculos.Models
+{
+    public class Veiculo
+    {
+        public int Id { get; set; }
+        public string Tipo { get; set; }
+
+        public decimal PesoCalculo { get; set; }
+    }
+}
